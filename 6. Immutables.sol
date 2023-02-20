d96ac8003f99805522bd640bbf5a33e2dd78c362
Sachin Shukla <sachinkshuklaoo7@gmail.com>
2023-02-20 10:55:07 +0530

    Updated filenames

--- 6. Immutables.sol
+++ 6. Immutables.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.17;
 
 /**
 Immutable
-Immutable variables are like constants. Values of immutable variables can be set inside the constructor but cannot be modified afterwards.
+Immutable variables are like constants. 
+Values of immutable variables can be set inside the constructor but cannot be modified afterwards.
  */
 
 contract Immutables {
